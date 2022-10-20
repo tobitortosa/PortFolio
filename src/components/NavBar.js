25ff2f405a72204d5063c9ed31abe410282913d8
Tobi Tortosa <tobiastortosa17@gmail.com>
2022-10-19 22:50:58 -0300

    added navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import s from "./NavBar.module.css";
+import { useState } from "react";
+
+export default function NavBar() {
+  const [hamburgerButton, setHamburgerButton] = useState(true);
+
+  const toggleButton = () => {
+    setHamburgerButton(!hamburgerButton);
+  };
+
+  return (
+    <div className={s.container}>
+      <nav className={s.navContainer}>
+        <div>
+          <a>
+            <div id={s.logoImg}></div>
+          </a>
+        </div>
+        <a
+          to="#"
+          className={hamburgerButton ? null : s.active}
+          onClick={toggleButton}
+          id={s.toggleButton}
+        >
+          <span id={s.firstBar} className={s.bar}></span>
+          <span id={s.secondBar} className={s.bar}></span>
+          <span id={s.thirdBar} className={s.bar}></span>
+        </a>
+        <div className={hamburgerButton ? s.toggle : s.ulContainer}>
+          {["Home", "About", "Skills", "Portfolio", "Contact"].map(
+            (el, index) => {
+              return (
+                <li key={index}>
+                  <a>
+                    <p>{el}</p>
+                  </a>
+                </li>
+              );
+            }
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+}
