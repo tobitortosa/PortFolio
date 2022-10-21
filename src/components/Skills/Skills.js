0b200e4a009a5d883799fc14eb82508ec254249c
Tobi Tortosa <tobiastortosa17@gmail.com>
2022-10-21 00:30:19 -0300

    added about

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,25 +1,228 @@
 import React from "react";
-import WaterCup from "../Cup/WaterCup";
 import s from "./Skills.module.css";
+import "./levels.css";
 
 export default function Skills() {
   return (
     <div className={s.container} id="Skills">
-      <WaterCup
-        waterLevel={"banner1"}
-        fillLevel={"fill1"}
-        waveLevel={"waveShape1"}
-      />
-      <WaterCup
-        waterLevel={"banner3"}
-        fillLevel={"fill3"}
-        waveLevel={"waveShape3"}
-      />
-      <WaterCup
-        waterLevel={"banner6"}
-        fillLevel={"fill6"}
-        waveLevel={"waveShape6"}
-      />
+      <div className={s.cupsContainer}>
+        <div className={s.cups}>
+          <div className={s.cup}>
+            <div id="banner1">
+              <div class="fill1">
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  width="300px"
+                  height="300px"
+                  viewBox="0 0 300 300"
+                  enable-background="new 0 0 300 300"
+                  xmlSpace="preserve"
+                >
+                  <path
+                    fill="#04ACFF"
+                    id="waveShape1"
+                    d="M300,300V2.5c0,0-0.6-0.1-1.1-0.1c0,0-25.5-2.3-40.5-2.4c-15,0-40.6,2.4-40.6,2.4
+                  c-12.3,1.1-30.3,1.8-31.9,1.9c-2-0.1-19.7-0.8-32-1.9c0,0-25.8-2.3-40.8-2.4c-15,0-40.8,2.4-40.8,2.4c-12.3,1.1-30.4,1.8-32,1.9
+                  c-2-0.1-20-0.8-32.2-1.9c0,0-3.1-0.3-8.1-0.7V300H300z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className={s.cup}>
+            <div id="banner1">
+              <div class="fill1">
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  width="300px"
+                  height="300px"
+                  viewBox="0 0 300 300"
+                  enable-background="new 0 0 300 300"
+                  xmlSpace="preserve"
+                >
+                  <path
+                    fill="#04ACFF"
+                    id="waveShape1"
+                    d="M300,300V2.5c0,0-0.6-0.1-1.1-0.1c0,0-25.5-2.3-40.5-2.4c-15,0-40.6,2.4-40.6,2.4
+                  c-12.3,1.1-30.3,1.8-31.9,1.9c-2-0.1-19.7-0.8-32-1.9c0,0-25.8-2.3-40.8-2.4c-15,0-40.8,2.4-40.8,2.4c-12.3,1.1-30.4,1.8-32,1.9
+                  c-2-0.1-20-0.8-32.2-1.9c0,0-3.1-0.3-8.1-0.7V300H300z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={s.cups}>
+          <div className={s.cup}>
+            <div id="banner1">
+              <div class="fill1">
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  width="300px"
+                  height="300px"
+                  viewBox="0 0 300 300"
+                  enable-background="new 0 0 300 300"
+                  xmlSpace="preserve"
+                >
+                  <path
+                    fill="#04ACFF"
+                    id="waveShape1"
+                    d="M300,300V2.5c0,0-0.6-0.1-1.1-0.1c0,0-25.5-2.3-40.5-2.4c-15,0-40.6,2.4-40.6,2.4
+                  c-12.3,1.1-30.3,1.8-31.9,1.9c-2-0.1-19.7-0.8-32-1.9c0,0-25.8-2.3-40.8-2.4c-15,0-40.8,2.4-40.8,2.4c-12.3,1.1-30.4,1.8-32,1.9
+                  c-2-0.1-20-0.8-32.2-1.9c0,0-3.1-0.3-8.1-0.7V300H300z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className={s.cup}>
+            <div id="banner1">
+              <div class="fill1">
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  width="300px"
+                  height="300px"
+                  viewBox="0 0 300 300"
+                  enable-background="new 0 0 300 300"
+                  xmlSpace="preserve"
+                >
+                  <path
+                    fill="#04ACFF"
+                    id="waveShape1"
+                    d="M300,300V2.5c0,0-0.6-0.1-1.1-0.1c0,0-25.5-2.3-40.5-2.4c-15,0-40.6,2.4-40.6,2.4
+                  c-12.3,1.1-30.3,1.8-31.9,1.9c-2-0.1-19.7-0.8-32-1.9c0,0-25.8-2.3-40.8-2.4c-15,0-40.8,2.4-40.8,2.4c-12.3,1.1-30.4,1.8-32,1.9
+                  c-2-0.1-20-0.8-32.2-1.9c0,0-3.1-0.3-8.1-0.7V300H300z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={s.cups}>
+          <div className={s.cup}>
+            <div id="banner1">
+              <div class="fill1">
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  width="300px"
+                  height="300px"
+                  viewBox="0 0 300 300"
+                  enable-background="new 0 0 300 300"
+                  xmlSpace="preserve"
+                >
+                  <path
+                    fill="#04ACFF"
+                    id="waveShape1"
+                    d="M300,300V2.5c0,0-0.6-0.1-1.1-0.1c0,0-25.5-2.3-40.5-2.4c-15,0-40.6,2.4-40.6,2.4
+                  c-12.3,1.1-30.3,1.8-31.9,1.9c-2-0.1-19.7-0.8-32-1.9c0,0-25.8-2.3-40.8-2.4c-15,0-40.8,2.4-40.8,2.4c-12.3,1.1-30.4,1.8-32,1.9
+                  c-2-0.1-20-0.8-32.2-1.9c0,0-3.1-0.3-8.1-0.7V300H300z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className={s.cup}>
+            <div id="banner1">
+              <div class="fill1">
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  width="300px"
+                  height="300px"
+                  viewBox="0 0 300 300"
+                  enable-background="new 0 0 300 300"
+                  xmlSpace="preserve"
+                >
+                  <path
+                    fill="#04ACFF"
+                    id="waveShape1"
+                    d="M300,300V2.5c0,0-0.6-0.1-1.1-0.1c0,0-25.5-2.3-40.5-2.4c-15,0-40.6,2.4-40.6,2.4
+                  c-12.3,1.1-30.3,1.8-31.9,1.9c-2-0.1-19.7-0.8-32-1.9c0,0-25.8-2.3-40.8-2.4c-15,0-40.8,2.4-40.8,2.4c-12.3,1.1-30.4,1.8-32,1.9
+                  c-2-0.1-20-0.8-32.2-1.9c0,0-3.1-0.3-8.1-0.7V300H300z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={s.cups}>
+          <div className={s.cup}>
+            <div id="banner1">
+              <div class="fill1">
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  width="300px"
+                  height="300px"
+                  viewBox="0 0 300 300"
+                  enable-background="new 0 0 300 300"
+                  xmlSpace="preserve"
+                >
+                  <path
+                    fill="#04ACFF"
+                    id="waveShape1"
+                    d="M300,300V2.5c0,0-0.6-0.1-1.1-0.1c0,0-25.5-2.3-40.5-2.4c-15,0-40.6,2.4-40.6,2.4
+                  c-12.3,1.1-30.3,1.8-31.9,1.9c-2-0.1-19.7-0.8-32-1.9c0,0-25.8-2.3-40.8-2.4c-15,0-40.8,2.4-40.8,2.4c-12.3,1.1-30.4,1.8-32,1.9
+                  c-2-0.1-20-0.8-32.2-1.9c0,0-3.1-0.3-8.1-0.7V300H300z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className={s.cup}>
+            <div id="banner1">
+              <div class="fill1">
+                <svg
+                  version="1.1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  xmlnsXlink="http://www.w3.org/1999/xlink"
+                  x="0px"
+                  y="0px"
+                  width="300px"
+                  height="300px"
+                  viewBox="0 0 300 300"
+                  enable-background="new 0 0 300 300"
+                  xmlSpace="preserve"
+                >
+                  <path
+                    fill="#04ACFF"
+                    id="waveShape1"
+                    d="M300,300V2.5c0,0-0.6-0.1-1.1-0.1c0,0-25.5-2.3-40.5-2.4c-15,0-40.6,2.4-40.6,2.4
+                  c-12.3,1.1-30.3,1.8-31.9,1.9c-2-0.1-19.7-0.8-32-1.9c0,0-25.8-2.3-40.8-2.4c-15,0-40.8,2.4-40.8,2.4c-12.3,1.1-30.4,1.8-32,1.9
+                  c-2-0.1-20-0.8-32.2-1.9c0,0-3.1-0.3-8.1-0.7V300H300z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
